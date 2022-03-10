781d9cf74034860ddb5c133cbb90e49bd4675ea7
rshelar5 <82907554+rshelar5@users.noreply.github.com>
2022-03-11 01:32:46 +0530

    removing stale files

--- docs/background.js
+++
@@ -1,6 +0,0 @@
-let color = '#FF0000';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cred', `color: ${color}`);
-});
