b7040af088e8fd12567641487d85dd4d66bd8e69
rshelar5 <82907554+rshelar5@users.noreply.github.com>
2022-09-20 19:36:43 +0530

    bug fix

--- src/app/interfaces/Product.ts
+++ src/app/interfaces/Product.ts
@@ -1,21 +1,20 @@
-export interface Product{
-  properShippingName: string,
-  hazmatClassification?: string,
-  hazmatCategory?: string,
-  classification?: string,
-  casnumber?: string,
-  packingGroup?: string,
-  unid?: string,
-  lehs?: string
+export interface Product {
+  properShippingName: string;
+  hazmatClassification?: string;
+  hazmatCategory?: string;
+  classification?: string;
+  casnumber?: string;
+  packingGroup?: string;
+  unid?: string;
+  lehs?: string;
 }
 
-export interface PureIngredient{
-  properShippingName: string,
-  hazmatClass?: string,
-  hazmatCategory?: string,
-  casnumber?: string,
-  packingGroup?: string,
-  unid?: string,
-  lehs?: string
+export interface PureIngredient {
+  properShippingName: string;
+  hazardClass?: string;
+  hazmatCategory?: string;
+  casnumber?: string;
+  packagingGroup?: string;
+  unid?: string;
+  lehs?: string;
 }
-
